ecb1157016f96c12cd37b88af3ef6c7080031e9e
Shifaa Barham <barhamshifaa@gmail.com>
2025-01-12 06:33:14 +0200

    update front

---
+++ frontend/src/components/Authentication/ProtectedRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+function ProtectedRoute({ children, allowedRoles }) {
+  const role = localStorage.getItem('role');
+  const token = localStorage.getItem('token');
+
+  if (!token || !allowedRoles.includes(role)) {
+    alert('You are not authorized to access this page!');
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
+
+
+
+export default ProtectedRoute;
